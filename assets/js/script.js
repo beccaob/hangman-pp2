002349d6d62dd590b9a53e4e9d58b6c80b2594c2
beccaob <rob3651@outlook.com>
2022-05-03 09:44:34 +0000

    add document query selector to grab elements in html file

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+'use strict' // restricts the use of undeclared variables
+
+// divs and buttons from html 
+const gameLetter = document.querySelector('.letters');
+const hintButton = document.querySelector('.hint-btn');
+const resetButton = document.querySelector('.reset-btn');
+const hint = document.querySelector('.hint');
+const hintText = document.querySelector('.hint-txt');
+const liveSpan = document.querySelector('.lives');
+const answer = document.querySelector('.answer');
+const scores = document.querySelector('.scores');
+const correctAns = document.querySelector('.correct');
+const correctWord = document.querySelector('.correct-word');
+const playAgain = document.querySelector('.reset-btn');
