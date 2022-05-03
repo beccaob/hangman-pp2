e857efb6386e9ecf2370aa60cd08d68b4e77fad3
beccaob <rob3651@outlook.com>
2022-05-03 09:51:16 +0000

    add game answers and hints

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,21 @@ const scores = document.querySelector('.scores');
 const correctAns = document.querySelector('.correct');
 const correctWord = document.querySelector('.correct-word');
 const playAgain = document.querySelector('.reset-btn');
+
+// capture letters & lives
+let letters; 
+let lives; 
+
+// game answers and hints
+const words = new Map([
+    ['ed sheeran', 'loves castles... and hills'],
+    ['beyonce', 'also known as queen b'],
+    ['kesha', 'brushes her teeth with a bottle of jack'],
+    ['stormzy', 'stars in Top Boy - a UK Drama'],
+    ['doja cat', 'has top hits such as woman, aint shit and kiss me more'],
+    ['ed sheeran', 'loves castles... and hills'],
+    ['ed sheeran', 'loves castles... and hills'],
+    ['ed sheeran', 'loves castles... and hills'],
+    ['ed sheeran', 'loves castles... and hills'],
+    ['ed sheeran', 'loves castles... and hills'],
+])
